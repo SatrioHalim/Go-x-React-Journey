9afc17728feb1e9495f585a4aaca166c4742be57
SatrioHalim <satriohalim21@gmail.com>
2026-01-19 23:46:49 +0700

    First init project

---
+++ golang-basic/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// fmt is a package that implements formatted I/O with functions analogous to C's printf and scanf.
+
+func main() {
+	fmt.Println("Hello, golang!")
+}
