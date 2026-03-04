a9d086f55093abca8079cacf43942c64c963eaca
SatrioHalim <satriohalim21@gmail.com>
2026-03-04 10:36:24 +0700

    Create config file

---
+++ study_case_project_management/config/config.go
@@ -0,0 +1,83 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB *gorm.DB
+	AppConfig *Config
+)
+
+type Config struct {
+	AppPort string
+	DBHost string
+	DBPort string
+	DBUser string
+	DBPassword string
+	DBName string
+	JWTSecret string
+	JWTExpireMinutes string
+	JWTRefreshToken string
+	JWTExpire string
+}
+
+func loadEnv(){
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found.")
+	}
+	AppConfig = &Config{
+		AppPort: getEnv("PORT","3030"),
+		DBHost: getEnv("DB_HOST", "localhost"),
+		DBPort: getEnv("DB_PORT", "5432"),
+		DBUser: getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD","password"),
+		DBName: getEnv("DB_NAME","project_management"),
+		JWTSecret: getEnv("JWT_SECRET","rahasia"),
+		JWTExpireMinutes: getEnv("JWT_EXPIRY_MINUTES","60"),
+		JWTRefreshToken: getEnv("REFRESH_TOKEN_EXPIRED","24h"),
+		JWTExpire: getEnv("JWT_EXPIRED","2h"),
+	}
+}
+
+func getEnv(key string, fallback string) string {
+	value,exist := os.LookupEnv(key)
+	if exist {
+		return value
+	} else {
+		return fallback
+	}
+}
+
+func connectDB(){
+	cfg := AppConfig
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname= %s sslmode=disable", cfg.DBHost, cfg.DBPort,cfg.DBUser,cfg.DBPassword,cfg.DBName)
+
+	// Open connection
+	db,err := gorm.Open(postgres.Open(dsn),&gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database",err)
+	}
+
+	// get db instance
+	sqlDB,err := db.DB()
+	if err != nil {
+		log.Fatal("Failed to get database instance",err)
+	}
+
+	// pulling connection
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	DB = db
+}
