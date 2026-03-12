50db5801ec82e7fc9a25a9bdada9c96c981ea7d2
SatrioHalim <satriohalim21@gmail.com>
2026-03-12 11:28:56 +0700

    Modify : Route => Using custom middleware for JWT | Add : Middleware for JWT protected groups route

--- study_case_project_management/routes/route.go
+++ study_case_project_management/routes/route.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/SatrioHalim/Go-x-React-Journey/controllers"
+	"github.com/SatrioHalim/Go-x-React-Journey/middleware"
 	"github.com/gofiber/fiber/v3"
 	"github.com/joho/godotenv"
 )
@@ -16,4 +17,10 @@ func Setup(app *fiber.App, uc *controllers.UserController){
 
 	app.Post("/v1/auth/register",uc.Register)
 	app.Post("/v1/auth/login",uc.Login)
+	
+	// JWT Protected routes : biar ga sembarangan orang bisa akses
+	api := app.Group("/api/v1", middleware.JWTAuth())
+
+	userGroup := api.Group("/users")
+	userGroup.Get("/:id",uc.GetUser) // /api/v1/users/:id
 }
