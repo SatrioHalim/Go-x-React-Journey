6eb79107b4aaa1be8df229878c292ef086bed4c0
SatrioHalim <satriohalim21@gmail.com>
2026-03-13 14:13:34 +0700

    Add User Pagination

--- study_case_project_management/repositories/user_repository.go
+++ study_case_project_management/repositories/user_repository.go
@@ -1,6 +1,8 @@
 package repositories
 
 import (
+	"strings"
+
 	"github.com/SatrioHalim/Go-x-React-Journey/config"
 	"github.com/SatrioHalim/Go-x-React-Journey/models"
 )
@@ -11,6 +13,7 @@ type UserRepository interface {
 	FindByEmail(email string) (*models.User,error)
 	FindByID(id uint)(*models.User,error)
 	FindByPublicID(publicID string)(*models.User,error)
+	FindAllPagination(filter,sort string, limit,offset int)([]models.User,int64,error)
 }
 
 type userRepository struct {}
@@ -39,4 +42,41 @@ func (r *userRepository) FindByPublicID(publicID string)(*models.User,error){
 	var user models.User
 	err := config.DB.Where("public_id = ?",publicID).First(&user).Error
 	return &user, err
+}
+
+func (r *userRepository) FindAllPagination(filter,sort string, limit,offset int)([]models.User,int64,error){
+	var users []models.User
+	var total int64
+
+	db := config.DB.Model(&models.User{})
+
+	// Filtering
+	if filter != ""{
+		filterPattern := "%" + filter + "%"
+		db = db.Where("name Ilike ? OR email Ilike ?",filterPattern,filterPattern) // Ilike => Case Insensitive, LIKE => Case sensitive
+	}
+	// Count total data
+	if err := db.Count(&total).Error ; err != nil {
+		return nil,0,err
+	}
+
+	// Sorting
+	if sort != "" {
+		// Ex, sort=name (ASC ascending), sort=-name(DSC descending)
+		if sort == "-id" {
+			sort = "-internal_id"
+		} else if sort == "id"{
+			sort = "internal_id"
+		}
+
+		if strings.HasPrefix(sort,"-"){
+			sort = strings.TrimPrefix(sort,"-") + " DESC"
+		} else {
+			sort += "ASC"
+		}
+		db = db.Order(sort)
+	}
+
+	err := db.Limit(limit).Offset(offset).Find(&users).Error
+	return users,total,err
 }
--- study_case_project_management/routes/route.go
+++ study_case_project_management/routes/route.go
@@ -22,5 +22,6 @@ func Setup(app *fiber.App, uc *controllers.UserController){
 	api := app.Group("/api/v1", middleware.JWTAuth())
 
 	userGroup := api.Group("/users")
+	userGroup.Get("/page",uc.GetUserPagination) // /api/v1/users/:id
 	userGroup.Get("/:id",uc.GetUser) // /api/v1/users/:id
 }
