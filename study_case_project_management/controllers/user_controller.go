63b65ab70467d252d4c9d12031c00cdf2a751a86
SatrioHalim <satriohalim21@gmail.com>
2026-03-17 14:03:31 +0700

    Add Delete user feature

--- study_case_project_management/controllers/user_controller.go
+++ study_case_project_management/controllers/user_controller.go
@@ -140,3 +140,11 @@ func (c *UserController) UpdateUser(ctx fiber.Ctx) error {
 	}
 	return utils.Success(ctx, "Update user successfully",userResp)
 }
+
+func (c *UserController) DeleteUser(ctx fiber.Ctx) error{
+	id,_ := strconv.Atoi(ctx.Params("id"))
+	if err := c.service.Delete(uint(id)); err != nil {
+		return utils.InternalServerError(ctx,"Fail to delete data",err.Error())
+	}
+	return utils.Success(ctx,"Data deleted successfully",id)
+}
--- study_case_project_management/routes/route.go
+++ study_case_project_management/routes/route.go
@@ -25,4 +25,5 @@ func Setup(app *fiber.App, uc *controllers.UserController){
 	userGroup.Get("/page",uc.GetUserPagination) // /api/v1/users/:id
 	userGroup.Get("/:id",uc.GetUser) // /api/v1/users/:id
 	userGroup.Put("/:id",uc.UpdateUser)
+	userGroup.Delete("/:id",uc.DeleteUser)
 }
