fa2517a4c3a997fcd0255b6d2e32ea604445f57e
SatrioHalim <satriohalim21@gmail.com>
2026-04-19 14:38:38 +0700

    Feat : Add delete & checklist task

--- js_react_dasar/todo_app/app.js
+++ js_react_dasar/todo_app/app.js
@@ -1,61 +1,83 @@
-const tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
+const tasks = localStorage.getItem("tasks")
+  ? JSON.parse(localStorage.getItem("tasks"))
+  : [];
 
 const taskList = document.getElementById("tasks-list");
 const taskForm = document.getElementById("task-form");
 
 taskForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    addTask();
-})
+  event.preventDefault();
+  addTask();
+});
 
-function addTask(){
-    const taskTitleValue = document.getElementById("task-title").value;
+function checkTask(index) {
+  const taskElement = document.getElementById(`task${index}`);
+  const checkboxElement = document.getElementById(`checkTask${index}`);
 
-    if(taskTitleValue.trim() == ""){
-        alert("Judul tugas tidak boleh kosong");
-        return;
-    };
-
-    const newTask = {
-        id : tasks.length + 1,
-        title : taskTitleValue.trim()
-    };
-
-    tasks.push(newTask);
-    document.getElementById("task-title").value = "";
-    localStorage.setItem("tasks",JSON.stringify(tasks));
-
-    renderTask();
+  if (checkboxElement.checked) {
+    taskElement.classList.add("checked");
+    tasks[index].checked = true;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  } else {
+    taskElement.classList.remove("checked");
+    tasks[index].checked = false;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 }
 
-function renderTask(){
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  renderTask();
+}
 
-    taskList.innerHTML = "";
-    if (tasks.length == 0){
-        const li = document.createElement("li");
-        li.classList.add("tasks-list-item");
-        li.innerHTML = `
+function addTask() {
+  const taskTitleValue = document.getElementById("task-title").value;
+
+  if (taskTitleValue.trim() == "") {
+    alert("Judul tugas tidak boleh kosong");
+    return;
+  }
+
+  const newTask = {
+    id: tasks.length + 1,
+    title: taskTitleValue.trim(),
+    checked: false,
+  };
+
+  tasks.push(newTask);
+  document.getElementById("task-title").value = "";
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  renderTask();
+}
+
+function renderTask() {
+  taskList.innerHTML = "";
+  if (tasks.length == 0) {
+    const li = document.createElement("li");
+    li.classList.add("tasks-list-item");
+    li.innerHTML = `
         <div class="task">
             <div class="task-title">
                 No activity yet
             </div>
         </div>
         `;
-        taskList.appendChild(li);
-        return;
+    taskList.appendChild(li);
+    return;
+  }
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.classList.add("tasks-list-item");
+    if (task.checked == true) {
+      li.classList.add("checked");
     }
-
-
-    tasks.forEach((task,index)=>{
-        const li = document.createElement("li");
-        li.classList.add("tasks-list-item")
-        if (task.checked == true){
-            li.classList.add("checked")
-        }
-        li.innerHTML = `
+    li.innerHTML = `
             <div class = "task">
                 <div>
-                    <input type="checkbox" onchange="checkTask(${index})" id="checkTask(${index})" ${task.checked} ? "checked=true" : ""/>
+                    <input type="checkbox" onchange="checkTask(${index})" id="checkTask${index}" ${task.checked ? 'checked' : ''}/>
                 </div>
                 <div class = "task-title">
                     ${task.title}
@@ -63,13 +85,13 @@ function renderTask(){
                 <button class ="button" onclick="deleteTask(${index})">Delete</button>
             </div>
         `;
-        li.id = `task${index}`;
-        taskList.appendChild(li)
-    });
+    li.id = `task${index}`;
+    taskList.appendChild(li);
+  });
 }
 
-function main(){
-    renderTask();
+function main() {
+  renderTask();
 }
 
 main();
