297d3bf161c0af827151af775fcc65336e6cee3a
SatrioHalim <satriohalim21@gmail.com>
2026-04-20 16:28:40 +0700

    Init Todo App with React

---
+++ js_react_dasar/todo_app/app-react.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <div>
+            <h1>Belajar React JS</h1>
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<App/>);
